460d38e27d818a16cb3877f8d3d2ba97eafa30f8
Gautam <gautam.gandhi.100@gmail.com>
2022-05-01 15:52:28 +0530

    initial commit

---
+++ src/main/java/co/offcampusjobs/OffCampusJobsApplication.java
@@ -0,0 +1,13 @@
+package co.offcampusjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffCampusJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffCampusJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/offcampusjobs/OffCampusJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.offcampusjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffCampusJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
