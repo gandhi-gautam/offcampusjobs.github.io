fec992fba3ba3130793103d39391f4cfaac8acc5
Gautam <gautam.gandhi.100@gmail.com>
2022-05-12 18:43:47 +0530

    Refactor Dto validation

--- src/main/java/co/offcampusjobs/dto/JobDto.java
+++ src/main/java/co/offcampusjobs/dto/JobDto.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 
