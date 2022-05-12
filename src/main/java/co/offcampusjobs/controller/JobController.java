84f9fdd35dbc45f9ccca85cd0432a5d14fa0fc61
sarthaksingh1551 <sarthak.singh1551@gmail.com>
2022-05-12 17:16:14 +0530

    Added Get mapping for returning the save job form

--- src/main/java/co/offcampusjobs/controller/JobController.java
+++ src/main/java/co/offcampusjobs/controller/JobController.java
@@ -2,6 +2,7 @@ package co.offcampusjobs.controller;
 
 import co.offcampusjobs.business.JobBusiness;
 import co.offcampusjobs.dto.JobDto;
+import co.offcampusjobs.model.Job;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +15,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class JobController {
     @Autowired
     private JobBusiness jobBusiness;
+
+    /**
+     * Scope : [This method returns a Save Job Form when url is '/job' with get mapping]
+     * Author : [Sarthak Singh]
+     * Comment : [refactoring date: 12-05-2022]
+     */
+    @GetMapping("/job")
+    public String saveJobForm(Model model){
+        model.addAttribute("jobDTO", new JobDto());
+        return "creator/SaveJob";
+    }
 
     /**
      * Scope : [This method saves Job when url is '/job' with post mapping]
